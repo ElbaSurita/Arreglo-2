7b313da47a2eb389339c2d9b6f88b46acf38d477
Alan Garcia <20251403@itla.edu.do>
2025-11-22 16:29:26 -0400

    Arreglando el Codigo

--- Arreglo#2/Program.cs
+++ Arreglo#2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 
 namespace ejercicio_arrays_DDR_2
 {
@@ -7,13 +8,15 @@ namespace ejercicio_arrays_DDR_2
         static void Main(string[] args)
         {
             //Alan Daniel Garcia Mendez / 2025-1403
-            int[] array = new int[8]; 
+            int[] array = new int[8];
+            int suma = 0;
 
             for (int i = 0; i < array.Length; i++)
             {
                 Console.WriteLine("Escribe un numero en la posición #" + (i + 1));
                 int num = Convert.ToInt32(Console.ReadLine());
                 array[i] = num;
+                suma = suma + num;
             }
 
             Console.WriteLine("Mostrando datos del Array:");
@@ -21,7 +24,7 @@ namespace ejercicio_arrays_DDR_2
             {
                 Console.WriteLine(array[i]);
             }
-
+            Console.WriteLine($"La suma de todos los numeros introducidos es igual a: {suma}");
             Console.ReadLine();
         }
     }
