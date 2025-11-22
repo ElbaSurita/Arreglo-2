5b88ecf976f98a3869d8f96a0c6b629c565ab79f
aland <aland@ALAN>
2025-11-22 15:15:49 -0400

    Agregar archivos de proyecto.

---
+++ Arreglo#2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ejercicio_arrays_DDR_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Alan Daniel Garcia Mendez / 2025-1403
+            int[] array = new int[8]; 
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine("Escribe un numero en la posición #" + (i + 1));
+                int num = Convert.ToInt32(Console.ReadLine());
+                array[i] = num;
+            }
+
+            Console.WriteLine("Mostrando datos del Array:");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
